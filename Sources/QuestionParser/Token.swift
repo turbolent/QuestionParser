555d77ec1e6116d7011f29ca64bea5fa0d10e581
Bastian Müller <bastian@turbolent.com>
2018-10-09 19:39:42 -0700

    add public initializer for Token

--- Sources/QuestionParser/Token.swift
+++ Sources/QuestionParser/Token.swift
@@ -4,6 +4,12 @@ public struct Token: Hashable {
     public let tag: String
     public let lemma: String
 
+    public init(word: String, tag: String, lemma: String) {
+        self.word = word
+        self.tag = tag
+        self.lemma = lemma
+    }
+
     private static let auxiliaryVerbLemmas: Set<String> =
         ["have", "be", "do"]
 
