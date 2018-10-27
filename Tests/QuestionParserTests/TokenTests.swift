041155ba8bc57f73d279ac0239ed582bb4b62017
Bastian Müller <bastian@turbolent.com>
2018-10-27 16:42:27 -0700

    add actual Token test

--- Tests/QuestionParserTests/TokenTests.swift
+++ Tests/QuestionParserTests/TokenTests.swift
@@ -1,32 +1,21 @@
-//
-//  TokenTests.swift
-//  QuestionParserTests
-//
-//  Created by Bastian Müller on 10/27/18.
-//
 
 import XCTest
+import QuestionParser
+import DiffedAssertEqual
 
 class TokenTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testEncoding() throws {
+        let tokens = [Token(word: "was", tag: "VBD", lemma: "be")]
+        let data = try JSONEncoder().encode(tokens)
+        guard let string = String(data: data, encoding: .utf8) else {
+            XCTFail("unable to decode JSON data")
+            return
         }
-    }
 
+        diffedAssertEqual(
+            string,
+            "[{\"tag\":\"VBD\",\"word\":\"was\",\"lemma\":\"be\"}]"
+        )
+    }
 }
